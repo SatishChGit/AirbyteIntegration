f50378fd7cbf73f69dd0e7188408e2c362b207f9
Satish Ch <satish.chinthanippu@teradata.com>
2022-11-01 18:05:28 +0530

    Initial dbt project

---
+++ models/git_assignees_db.sql
@@ -0,0 +1,16 @@
+{{ config(
+    indexes = [{'columns':['_airbyte_emitted_at'],'type':'btree'}],
+    unique_key = '_airbyte_ab_id',
+    tags = [ "top-level-intermediate" ]
+) }}
+select
+    CAST (_airbyte_data.JSONExtract('$.id')  AS VARCHAR(200))  as id,
+    CAST (_airbyte_data.JSONExtract('$.login')   AS VARCHAR(200)) as username,
+     CAST (_airbyte_data.JSONExtract('$.node_id')    AS VARCHAR(200))  as nodeid,
+     CAST (_airbyte_data.JSONExtract('$.avatar_url')  AS VARCHAR(200)) as avatarurl,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    {{ current_timestamp() }} as _airbyte_normalized_at
+from {{ source('airbyte_quickstartstd_repo_01Nov2022', '_airbyte_raw_assignees') }} as table_alias
+-- data_stream
+where 1 = 1
